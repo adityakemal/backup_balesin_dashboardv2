82617b26978431e3d37bb1b08e0de7f9126ccf53
Kemal Aditya Zulfikar <kadityafikar@gmail.com>
2023-02-14 17:10:39 +0700

    add login style

--- src/features/auth/AuthContainer.jsx
+++ src/features/auth/AuthContainer.jsx
@@ -1,17 +1,29 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React from "react";
+import { useSelector } from "react-redux";
+import Login from "./components/Login";
+import Register from "./components/Register";
 
-import Login from './components/Login'
-import Register from './components/Register'
-
-
+import logo from "../../images/balesinLogoWhite.png";
+import wave from "../../images/wave.svg";
 
 export default function AuthContainer() {
-    const { isRegister } = useSelector(state => state.auth)
+  const { isRegister } = useSelector((state) => state.auth);
 
-    return (
-        <div className='container auth h-100'>
-            <Login />
+  return (
+    <div style={{ backgroundImage: `url(${wave})` }} className="auth ">
+      <div className="container-fluid d-flex align-items-center justify-content-center position-relative h-100">
+        <div className="w-100 text-center logo">
+          <img src={logo} alt="" />
         </div>
-    )
+
+        <div className=" box py-4 px-5 bg-white">
+          <h3 className="mb-2 text-center">Welcome back 👋</h3>
+          <p className="mb-4 text-center light">
+            Sign in with your Balesin account
+          </p>
+          <Login />
+        </div>
+      </div>
+    </div>
+  );
 }
